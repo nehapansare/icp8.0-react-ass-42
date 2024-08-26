9db36d9296796d8269e33578dd9fadda0f4b43cb
nehapansare <nehapansare007@gmail.com>
2024-08-26 16:45:31 +0530

    index.html file added

---
+++ src/config/data.js
@@ -0,0 +1,58 @@
+const TITLE="Pizza Palazzo"
+
+const THEME ={
+    main:"#eff0f3",
+    highlight:"#ff8e3c",
+    secondary:"#ff5e78",
+    tertiary:"#d9376e",
+    dark:"#0d0d0d",
+}
+
+const ITEM=[{
+
+        imgUrl:"https://img.freepik.com/free-photo/slice-crispy-pizza-with-meat-cheese_140725-6974.jpg?t=st=1724433569~exp=1724437169~hmac=4e30fb986c566b417afa089f6da4e1e21d092c19acdbbb6daa62c8e8e0d25be9&w=740",
+        title:"Cheesy Pizza",
+        Discription:"Capsicum,Onion made more delicious",
+        price:"150"
+
+    },
+    {
+        imgUrl:"https://img.freepik.com/free-photo/meat-pizza-with-red-onion-rings-olive-cheese_140725-1979.jpg?t=st=1724435239~exp=1724438839~hmac=eb6a2b0046e3143b072786f027c08549e26e79e739854243b5f6a119c8ee0ff0&w=900",
+        title:"Peppy Paneer",
+        Discription:"Paneer,crisp capsicum with spicy red paprika",
+        price:"350"
+
+    },
+    {
+        imgUrl:"https://img.freepik.com/premium-photo/heart-shaped-tandoori-chicken-pizza-wooden-plate_13329-672.jpg?w=1060",
+       title:"Indi Tandoori Paneer",
+       Discription:"It is hot.it is spicy. It is Oh-so-Indian. Tandoori Paneer with capsicum,red Paprika and Mint Mayo",
+        price:"200"
+
+    },
+    {
+        imgUrl:"https://img.freepik.com/free-photo/pizza-food-food_144627-38490.jpg?t=st=1724435502~exp=1724439102~hmac=488eaf0728ef4ccf3489bb48f634d6b75c8900ec2bb6a721a969c3bf1cf4cc5c&w=360",
+       title:"Veggie Paradise",
+       Discription:"The awesome foursome! Golden black olive capsicum red paprika",
+        price:"450"
+
+    },
+    {
+        imgUrl:"https://img.freepik.com/free-photo/closeup-whole-delicious-pizza-with-melted-cheese-corn-onions-board_181624-46716.jpg?t=st=1724435584~exp=1724439184~hmac=7ac2c279382b06169f835883e6d014018593e297977c09908d932217d0872797&w=1060",
+        title:"Cheesee and corn",
+        Discription:"A Deleteable combination of sweet and juicy Golden Corn",
+         price:"280"
+    },
+    {
+        imgUrl:"https://img.freepik.com/free-photo/imeretian-khachapuri-cheese-lemon-side-view_140725-11276.jpg?t=st=1724435656~exp=1724439256~hmac=1ffb0b8bd96f322382ff923c5932ac3f048809671a98a4ceda7f22bd87ef956d&w=900",
+       title:"Double cheese margherita",
+       Discription:"A Classic delight loaded with extra 100% Mozilla Cheese",
+        price:"300"
+
+    }
+]
+export{
+    ITEM,
+    TITLE,
+    THEME
+}
