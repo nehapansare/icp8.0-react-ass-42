50814062dd09af7f1d5fb2c95bdbbe9c3bbb01b9
nehapansare <nehapansare007@gmail.com>
2024-08-26 16:53:57 +0530

    footer.js file added

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+       All right reserved &copy; 2024
+    </div>
+  )
+}
+
+export default Footer
