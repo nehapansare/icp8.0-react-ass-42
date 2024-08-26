62dcfd21e4ee708280b92de13962a235a49c7d7c
nehapansare <nehapansare007@gmail.com>
2024-08-26 16:52:21 +0530

    navbarmenu.js file added

---
+++ src/component/Menu Nav/MenuNavbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./MenuNavbar.css"
+
+function MenuNavbar() {
+  return (
+
+      
+<div  className='navbar2'>
+        <div>
+            <div className='title'>Menu</div>
+            <a href="/veg" className="navbar2-link">Veg Pizza</a>
+            <a href="/nonveg" className="navbar2-link">Non-Veg Pizza</a>
+         </div>
+</div>
+
+
+  )
+}
+
+export default MenuNavbar
